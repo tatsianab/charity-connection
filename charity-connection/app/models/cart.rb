ebf0beb9909b9e89bba0c4a869a2ddec2f249f20
NColey <nrcoley@gmail.com>
2015-12-01 18:15:16 -0500

    add item and total methods to cart model and change order status and change inventory methods to order model

--- charity-connection/app/models/cart.rb
+++ charity-connection/app/models/cart.rb
@@ -1,2 +1,26 @@
 class Cart < ActiveRecord::Base
+	has_many :line_items
+	has_many :items, through: :line_items
+
+	def add_item(item_id)
+		item = Item.find(item_id)
+		if self.items.include?(item)
+			line_item = LineItem.find_by({item_id: item_id, cart_id: self.id})
+			line_item.quantity +=1
+		else
+			self.items << item
+			line_item = LineItem.find_by({item_id: item_id, cart_id: self.id})
+			line_item.quantity = 1
+		end
+		line_item.save
+		line_item
+	end
+
+	def total
+		total = 0
+		self.items.each do |item|
+			total += item.price
+		end
+		total
+	end
 end
--- charity-connection/app/models/order.rb
+++ charity-connection/app/models/order.rb
@@ -1,4 +1,20 @@
 class Order < ActiveRecord::Base
 	belongs_to :cart
 	has_many :items, through: :cart
+
+	def self.create_from_cart(cart)
+		@order = Order.find_or_create_by({id: cart.id, total: cart.items.count, cart_id: cart.id})
+	end
+
+	def change_order_status
+		self.status = "submitted"
+	end
+
+	def change_inventory
+		cart.items.each do |item|
+			item = Item.find(item.id)
+			item.inventory -=1
+			item.save
+		end
+	end
 end
