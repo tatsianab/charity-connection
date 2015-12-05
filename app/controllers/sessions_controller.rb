9ebb0c309af60d62c69d9f95016b137b87e6b9cf
NColey <nrcoley@gmail.com>
2015-12-04 21:49:30 -0500

    updates flash message upon empty login form error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to @user
     else
-      flash[:notice] = "Invalid username or password"
+      flash[:error] = "Invalid username or password"
       render "new"
     end
   end
