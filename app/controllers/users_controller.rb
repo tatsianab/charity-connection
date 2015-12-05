99e222d8a05b5cb768c2c956a3532f8401de2f13
NColey <nrcoley@gmail.com>
2015-12-05 00:56:17 -0500

    refactors users controller to move create business and charity logic to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,14 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		organization = params[:organization]
 	    @user = User.new(user_params)
 	    if @user.save
+	       @user.create_business_or_charity(@user, organization)
 	       log_in(@user)
 	       redirect_to @user
 	    else
+	       flash[:alert] = "Sorry, your account could not be created."
 	       render 'new'
 	    end
   	end
@@ -18,7 +21,7 @@ class UsersController < ApplicationController
 		@user = User.find_by_id(params[:id])
 
 		if @user.nil?
-			flash[:notice] = "User was not found, please login or sign up"
+			flash[:notice] = "User was not found, please login or sign up."
 			redirect_to signup_path
 		end
 
@@ -27,6 +30,7 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-      params.require(:user).permit(:name, :email, :address, :zip, :phone, :city, :state, :organization, :password, :password_confirmation)
+    	params.require(:user).permit(:name, :email, :address, :zip, :phone, :city, :state, :organization, :password, :password_confirmation)
     end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
-  has_one :business
-  has_one :charity
-  has_one :admin
-  validates_presence_of :name, :email, :address, :zip, :phone, :city, :state
-  validates :email, format: {:with => /@/}
-  has_secure_password
+	has_one :business
+	has_one :charity
+	has_one :admin
+	validates_presence_of :name, :email, :address, :zip, :phone, :city, :state
+	validates :email, format: {:with => /@/}
+	has_secure_password
+
+	def create_business_or_charity(user, organization)
+		Object.const_get(organization).create(user_id: user.id)
+	end
 
 end
