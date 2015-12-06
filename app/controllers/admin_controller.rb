2785004e1dccf6a75b38e2d82d7dae46e3845f52
NColey <nrcoley@gmail.com>
2015-12-06 18:32:21 -0500

    adds admin portal link to header

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,14 @@
 class AdminController < ApplicationController
 
 	def show
+		@user = User.find_by_id(params[:id])
+
+		if @user.nil?
+			flash[:notice] = "User was not found, please login or sign up."
+			redirect_to signup_path
+		elsif !@user.admin
+			flash[:alert] = "I'm sorry, you must be registered as an admin to view this page."
+			redirect_to signup_path	
+		end
 	end
 end
